03e2b07c5569bc060d9fddc583694581d8a5661b
dgsw51 <lkj.100912@gmail.com>
2026-08-30 18:46:22 +0900

    Feat: repository 작성

--- src/main/java/com/rudwns/homework/auth/repository/UserRepository.java
+++ src/main/java/com/rudwns/homework/auth/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.rudwns.homework.auth.repository;
 import com.rudwns.homework.auth.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
