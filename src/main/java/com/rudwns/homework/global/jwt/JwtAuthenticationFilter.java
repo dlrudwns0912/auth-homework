51477a6b9f606a768b249d43373bcf930009210c
이경준 <lkj.100912@gmail.com>
2026-09-12 21:25:43 +0900

    fix: JwtAuthenticationFilter 수정

--- src/main/java/com/rudwns/homework/global/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/rudwns/homework/global/jwt/JwtAuthenticationFilter.java
@@ -1,21 +1,16 @@
 package com.rudwns.homework.global.jwt;
 
+import com.rudwns.homework.domain.auth.entity.UserRole;
 import com.rudwns.homework.domain.auth.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import java.io.IOException;
-import java.util.Collections;
 
-@Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
@@ -23,24 +18,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final UserRepository userRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        String header = req.getHeader("Authorization");
+        String header = request.getHeader("Authorization");
 
         if (header != null && header.startsWith("Bearer ")) {
             try {
                 String token = header.substring(7);
                 String email = jwtProvider.getEmail(token);
 
-                userRepository.findByEmail(email).ifPresent(user -> {
+                userRepository.findByEmail(email).isPresent(user -> {
                     var authority = new SimpleGrantedAuthority("ROLE_" + user.getRole().name());
-                    var auth = new UsernamePasswordAuthenticationToken(email, null, Collections.singletonList(authority));
-                    SecurityContextHolder.getContext().setAuthentication(auth);
-                });
-            } catch (Exception ignored) {}
+                })
+            } catch ()
         }
-
-        chain.doFilter(req, res);
     }
 }
